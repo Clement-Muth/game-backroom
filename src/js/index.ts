1816c76b8b4d2eb44b5ce8ebfaf10b9be2523194
Clément Muth <clementmuth@gmail.com>
2024-03-18 01:18:21 -0700

    feat: create game engine json (#33)

--- src/js/index.ts
+++ src/js/index.ts
@@ -5,41 +5,11 @@ import MenuScene from "./scenes/menu";
 (async () => {
   const app = new Application();
 
-  app.init({ background: "transparent", resizeTo: window });
+  await app.init({ background: "transparent", resizeTo: window });
 
   await Assets.load([
-    {
-      alias: "background",
-      src: "/public/static/images/menu/menu.jpg",
-    },
-    {
-      alias: "map1",
-      src: "/public/static/images/game/map/map1.jpeg",
-    },
-    {
-      alias: "map2",
-      src: "/public/static/images/game/map/map6.jpeg",
-    },
-    {
-      alias: "map3",
-      src: "/public/static/images/game/map/map12.jpeg",
-    },
-    // {
-    //   alias: "map4",
-    //   src: "/public/static/images/game/map/map4.jpeg",
-    // },
-    // {
-    //   alias: "map5",
-    //   src: "/public/static/images/game/map/map5.jpeg",
-    // },
-    // {
-    //   alias: "map6",
-    //   src: "/public/static/images/game/map/map6.jpeg",
-    // },
-    // {
-    //   alias: "map7",
-    //   src: "/public/static/images/game/map/map7.jpeg",
-    // },
+    app.dataEngine.assets.menu.background,
+    ...app.dataEngine.assets.map,
   ]);
 
   const menuScene = new MenuScene(app.screen.width, app.screen.height, app);
